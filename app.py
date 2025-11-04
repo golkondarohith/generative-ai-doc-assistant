7262651de19e5531746944e70feb8de3816589cc
Rohith Golkonda <rohithpatel105@gmail.com>
2025-11-04 01:18:20 -0500

    Add text chunks and vectorstore to the pdf file

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 import streamlit as st
 from dotenv import load_dotenv
+from pypdf import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n", 
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
 
 def main():
     load_dotenv()
@@ -13,13 +42,13 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing"):
                 #get the pdf
-
-
-                #get the text chunks
-
-
+                raw_text = get_pdf_text(pdf_docs)
+                # st.write(raw_text)
+                #get text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
                 #create vector store
-
+                vectorstore = get_vectorstore(text_chunks)
 
 if __name__ == '__main__':
     main()
