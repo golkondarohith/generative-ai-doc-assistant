4ca6026ee13b4f618a653305b324066084c481f2
Rohith Golkonda <rohithpatel105@gmail.com>
2025-11-07 00:42:32 -0500

    Extracted text from the Image using OCR in a pdf.

--- practice3.py
+++ practice3.py
@@ -9,18 +9,20 @@ def extract_text(image: Image.Image):
 
 def extract_text_from_image(pdf_path):
     reader = PdfReader(pdf_path)
-    for page in reader.pages:
-        for image_file_object in page.images:
+    for page_number, page in enumerate(reader.pages, start=1):
+        for image_number, image_file_object in enumerate(page.images, start=1):
             image = Image.open(io.BytesIO(image_file_object.data)).convert("RGB")
 
 
             # Step 1
             text_content = extract_text(image)
+            print(f"Page Number {page_number}")
+            print(f"Image Number {image_number}")
             print("Extracted Text:\n")
-            print(text_content if text_content else "(No text found)")
+            print(text_content if text_content else "(No text found)\n")
 
 
 
 
-pdf_path = "html2pdfx_Report_sample.pdf"
+pdf_path = "Software process workflows and Iteration workflows.pdf"
 extract_text_from_image(pdf_path)
